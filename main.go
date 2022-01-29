aa6f2dafd9d46a91aaae3fc0b8702e5c36bd6388
zriyansh <prik2two@gmail.com>
2022-01-29 18:54:41 +0530

    multi-threading implemented

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -17,26 +18,32 @@ type Point2d struct {
 }
 
 var (
-	r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	r         = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	waitGroup = sync.WaitGroup{}
 )
 
-func findArea(pointStr string) {
-	var points []Point2d
-	regexData := r.FindAllStringSubmatch(pointStr, -1)
+const noOfThreads int = 18
 
-	for _, p := range regexData {
-		x, _ := strconv.Atoi(p[1])
-		y, _ := strconv.Atoi(p[2])
-		points = append(points, Point2d{x, y})
-	}
+func findArea(inputChannel chan string) {
+	for pointStr := range inputChannel {
+		var points []Point2d
+		regexData := r.FindAllStringSubmatch(pointStr, -1)
 
-	area := 0.0
-	for i := 0; i < len(points); i++ {
-		a, b := points[i], points[(i+1)%len(points)]
-		// % is used so that we do not cross the limit of loop by doing i+1
-		area += float64(a.x*b.y) - float64(a.y*b.x)
+		for _, p := range regexData {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2d{x, y})
+		}
+
+		area := 0.0
+		for i := 0; i < len(points); i++ {
+			a, b := points[i], points[(i+1)%len(points)]
+			// % is used so that we do not cross the limit of loop by doing i+1
+			area += float64(a.x*b.y) - float64(a.y*b.x)
+		}
+		fmt.Println(math.Abs(area) / 2.0)
 	}
-	fmt.Println(math.Abs(area) / 2.0)
+	waitGroup.Done()
 }
 
 func main() {
@@ -44,11 +51,20 @@ func main() {
 	dat, _ := ioutil.ReadFile(filepath.Join(absPath, "polygons.txt"))
 	text := string(dat)
 
+	inputChannel := make(chan string, 1000)
+	for i := 0; i < noOfThreads; i++ {
+		go findArea(inputChannel)
+	}
+	waitGroup.Add(noOfThreads)
+
 	start := time.Now()
 	for _, line := range strings.Split(text, "\n") {
 		// split the text string file whenever a new line appears
-		findArea(line)
+		// findArea(line)
+		inputChannel <- line
 	}
+	close(inputChannel) // signal to worker threads that work is finished
+	waitGroup.Wait()
 	elapsed := time.Since(start)
 	fmt.Printf("time taken %s", elapsed)
 }
